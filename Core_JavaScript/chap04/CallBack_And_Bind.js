a837f1a1eeb8168a35dfdb525382ab164a84d650
RootSong <sgh8774@gmail.com>
2022-02-02 22:33:00 +0900

    Added core-javascript on CallBack function and Asynchronous process.

---
+++ Core_JavaScript/chap04/CallBack_And_Bind.js
@@ -0,0 +1,12 @@
+const obj1 = {
+    name: 'obj1',
+    func: function () {
+        console.log(this.name);
+    }
+};
+setTimeout(obj1.func.bind(obj1), 1000);
+
+const obj2 = {
+    name: 'obj2'
+};
+setTimeout(obj1.func.bind(obj2),1500);
